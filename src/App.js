e0eac5fb4d7ed22f92812e829f59099596059f1d
PraveenTSG <praveen@thirdspaceglobal.com>
2022-08-24 20:15:02 +0530

    configured routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App">hello</div>;
+import { Routes, Route } from "react-router-dom";
+import Test from "./pages/Test";
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
