81e0b51b3f5a6a4209e908791ea251286a0c9e96
PraveenTSG <praveen@thirdspaceglobal.com>
2022-08-24 20:09:46 +0530

    Initial commit

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQjTuO2Wbmz77Tq9ekyhHl6jd9RW_mEcw",
+  authDomain: "fitflame-82992.firebaseapp.com",
+  projectId: "fitflame-82992",
+  storageBucket: "fitflame-82992.appspot.com",
+  messagingSenderId: "1091559323626",
+  appId: "1:1091559323626:web:d903f7529d799cbd1ca493",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
